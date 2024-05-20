c121c865c99a868056f4611f30256667c9c21c67
dhirajpatra <dhiraj.patra@gmail.com>
2024-05-20 12:21:09 +0530

    updated with async call

--- routers/batch_router.py
+++ routers/batch_router.py
@@ -8,12 +8,16 @@ batch_router = APIRouter()
 
 
 @batch_router.post("/add_numbers")
-def add_numbers_endpoint(numbers: List[int]) -> List[int]:
+# both async and sync we can call depends on our requirement
+# if it is io heavy then can make async is useful
+async def add_numbers_endpoint(numbers: List[int]) -> List[int]:
     """Endpoint to perform addition on a list of integers."""
     return add_numbers(numbers)
 
 
 @batch_router.post("/process_batch", response_model=BatchResponse)
+# both async and sync we can call depends on our requirement
+# if it is io heavy then can make async is useful
 async def process_batch_endpoint(batch: BatchRequest) -> BatchResponse:
     """Endpoint to process batch using multiprocessing pool."""
     try:
